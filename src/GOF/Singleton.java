b9b8816aa8923b355b16981c200d9670f25188b5
sg-kbhatt <kbhatt@sgintl.com>
2023-06-04 20:17:24 +0530

    singleton double-checked locking approach

---
+++ src/GOF/Singleton.java
@@ -0,0 +1,25 @@
+package GOF;
+//double-checked locking
+public final class Singleton {
+    static int numberOfInstances=0;
+    private static Singleton singleton ;
+    private Singleton(){
+        numberOfInstances++;
+        System.out.println("Number of instances at this moment = "+numberOfInstances);
+    }
+    public static  Singleton getSingleton(){
+        if(singleton==null){
+            synchronized (Singleton.class){
+                if(singleton==null){
+                    singleton= new Singleton();
+                    System.out.println("New captain selected for team!!");
+                }else{
+                    System.out.println("Singleton class already have one instance use that!!");
+                }
+            }
+        }
+        return singleton;
+    }
+       }
+
+
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,17 @@
-public class Main {
+import GOF.Singleton;
+
+public class Main{
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("***Singleton pattern Demo\n");
+        System.out.println("Trying to make a singleton object");
+        //lazy initialization
+        Singleton singleton1= Singleton.getSingleton();
+        //lazy initialization
+        Singleton singleton2 = Singleton.getSingleton();
+        if(singleton1== singleton2){
+            System.out.println("singleton1 and 2 are same instances");
+        }
+
     }
+
 }
